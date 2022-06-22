cfe7a8a226d68db534481586c16866d43a8efba4
Noga <nogaran01@gmail.com>
2022-06-22 16:39:12 +0300

    print name of loaction

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -20,7 +20,9 @@ function initMap(lat = 32.0749831, lng = 34.9120554) {
             })
             console.log('Map!', gMap);
             gMap.addListener("click", (e) => {
-            panTo(e.latLng);
+                // console.log(e.latLng.toJSON());
+                findLocationName(e.latLng.toJSON())
+                panTo(e.latLng);
             })
         })
 }
@@ -42,7 +44,7 @@ function panTo(lat, lng) {
 
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = 'AIzaSyAFIrpyP3UsU9Zqgkv97bPzLIo3mNrpaKI'; //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyAFIrpyP3UsU9Zqgkv97bPzLIo3mNrpaKI'; //Done: Enter your API Key
     var elGoogleApi = document.createElement('script');
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     elGoogleApi.async = true;
@@ -52,4 +54,18 @@ function _connectGoogleApi() {
         elGoogleApi.onload = resolve;
         elGoogleApi.onerror = () => reject('Google script failed to load')
     })
+}
+
+function findLocationName({lat,lng}) {
+    // console.log(lat, lng);
+
+    const API_KEY = 'AIzaSyA2AxIb85Vl7Ms8mi7l3iE4njCWjR9nkCQ'; //Done: Enter your API Key
+    const latLan=lat+', '+lng
+    const prm = fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${latLan}&sensor=true&key=${API_KEY}`).then(res => res.json())
+    prm.then(printLoc)
+}
+
+function printLoc(location){
+    console.log(location.results[0].formatted_address,'\n',location.results[0].place_id,'\n',location.results[0].geometry.location);
+    // saveLocation(location.results[0].formatted_address,location.results[0].id)
 }
